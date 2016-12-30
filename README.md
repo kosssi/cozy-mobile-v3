1d2ebcd2c9367a6eb98a7d4ffa942e9ac91c677e
Simon Constans <simonc@linux.com>
2016-12-30 08:26:22 +0100

    First commit from https://github.com/uhlryk/my-cordova-react-boilerplate :+1:

---
+++ README.md
@@ -0,0 +1,134 @@
+# boilerplate cordova react app
+
+## Installation:
+
+How to use this code as new app:
+
+*This is ES6 & ES7 babel version. If you prefere you can typescript version then follow instructions from this
+[link](https://github.com/uhlryk/my-cordova-react-boilerplate/tree/typescript)*
+
+1 Clone this repository to new empty dir:
+
+    git clone https://github.com/uhlryk/my-cordova-react-boilerplate.git emptyDirectory
+
+2 Create your app repository
+
+3A Change remote git url to your repository url
+
+    git remote set-url origin https://github.com/yournick/your-app-url.git
+
+3B If you don't want to have this boilerplate commit history you can remove it and create new own git init:
+
+    rm -rf .git
+    git init
+    git remote add origin https://github.com/yournick/your-app-url.git
+    git add .
+    git commit
+
+4 change app info in package.json
+
+5 update npm modules
+
+    npm install
+
+## React Hot loader:
+
+WebpackDevServer is using network ip, therefore you can use it from any device in current network.
+
+## Commands:
+
+First time there is need to build cordova init app. It could be run only one, before any other commands:
+
+    gulp init-cordova
+
+Compile/transpile/transform all source files and copy them to cordova app
+
+    gulp prepare-build
+
+Build cordova app. Should be run only if cordova app is initialized
+
+    gulp build-ios
+
+    gulp build-android
+
+    gulp build-browser
+
+Build and emulate cordova app. Should be run only if cordova app is initialized
+
+    gulp emulate-ios
+
+    gulp emulate-android
+
+## Use case:
+
+If you just install this boilerplate, and made some chanages in src/ and want to run ios emulation:
+
+    gulp init-cordova && gulp prepare-build && gulp emulate-ios
+
+If you initialized cordova previously and you want to crate android app:
+
+    gulp prepare-build && gulp emulate-android
+
+## Hot loader emulation
+
+It allows to make changes in react app and make it instantly available in ios/android emulator (cordova need to be initialized)
+
+    gulp prebuild-ios-hot
+
+    gulp prebuild-android-hot
+
+    gulp prebuild-browser-hot
+
+## Ripple emulation with hot loader
+
+*A browser based html5 mobile application development and testing tool*
+
+Cordova need to be initialized. To run this emulator:
+
+    gulp prebuild-ripple-hot
+
+## The PhoneGap Developer App with hot loader
+*Develop locally then see the changes instantly on your mobile device.*
+
+To run test app you have to install:
+
+for android:
+
+    https://play.google.com/store/apps/details?id=com.adobe.phonegap.app
+
+for ios:
+
+    https://itunes.apple.com/app/id843536693
+
+And run on phone this app, then run :
+
+    gulp prebuild-phonegap-hot
+
+And write in phone app phonegap server listening url (it should be at the beginning of logs something like this `[phonegap] listening on 192.168.1.100:3001`)
+
+## Structure:
+
+  * src                - directory with all sources
+  * release            - directory with creaded electron app (created automatically)
+  * package.json       - standard npm package. But name and version will be copied to electron npm. THere is also field `electronVersion`
+  * src/index.jsx      - root react file
+  * src/index.html     - app layuot
+  * src/components     - place here all components
+  * src/reducers       - place here all redux reducers
+
+## Technology:
+
+  * cordova/phonegap
+  * react
+  * react-router
+  * jsx
+  * redux
+  * hot-loader
+  * webpack
+  * ES6 & ES7
+  * gulp
+  * compass
+
+## Licence:
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,188 @@
+var gulp = require('gulp');
+var gutil = require("gulp-util");
+var shell = require('gulp-shell')
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var runSequence = require('run-sequence');
+var path = require("path");
+var preprocess = require('gulp-preprocess');
+var packageJson = require('./package.json');
+var clean = require('gulp-clean');
+var myip = require('quick-local-ip');
+
+const WEBPACK_NETWORK_IP = myip.getLocalIP4();
+const WEBPACK_SERVER_HOST = 'http://' + WEBPACK_NETWORK_IP;
+const WEBPACK_SERVER_PORT = 3000;
+const PHONEGAP_SERVER_PORT = 3001;
+const STATIC_PATH = 'static';
+const BUNDLE_FILE = 'bundle.js';
+const APP_NAME = packageJson.name;
+const APP_ID = packageJson.id;
+const APP_VERSION = packageJson.version;
+
+var webpackOptionsLoader = {
+  test: /.jsx?$/,
+  loaders: ['babel?presets[]=react,presets[]=es2015,presets[]=stage-0'],
+  include: path.join(__dirname, 'src'),
+  exclude: /node_modules/
+};
+var webpackOptions = {
+  module: {
+    loaders: [
+      webpackOptionsLoader,
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  },
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, './www/' + STATIC_PATH + '/'),
+    filename: BUNDLE_FILE
+  },
+  eslint: {
+
+  },
+  debug: true,
+  progress: false,
+  emitError: true,
+  emitWarning: true,
+  failOnError: true,
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  devtool: 'source-map'
+};
+
+/**
+ * copy non bundled files from src to dist directory
+ */
+gulp.task('copy-layout', function() {
+  return gulp.src(['./src/index.html'])
+    .pipe(preprocess({
+      context: {
+        BUNDLE_PATH: './' + STATIC_PATH + '/' + BUNDLE_FILE,
+        APP_NAME: APP_NAME
+      }
+    }))
+    .pipe(gulp.dest('./www'))
+});
+
+/**
+ * copy non bundled files from src to dist directory with path to hot loader server
+ */
+gulp.task('copy-layout-hot', function() {
+  return gulp.src(['./src/index.html'])
+    .pipe(preprocess({
+      context: {
+        BUNDLE_PATH: WEBPACK_SERVER_HOST + ':' + WEBPACK_SERVER_PORT +'/' + STATIC_PATH + '/' + BUNDLE_FILE,
+        APP_NAME: APP_NAME
+      }
+    }))
+    .pipe(gulp.dest('./www'))
+});
+
+/**
+ * Compile react jsx ES6 & ES7 to ES5 js
+ */
+gulp.task('compile-react', function(done) {
+  webpack(webpackOptions, function(err, stats) {
+    if(err) console.log(err);
+    gutil.log("[webpack]", stats.toString({}));
+    done();
+  });
+});
+
+/**
+ * Compile react jsx ES6 & ES7 to ES5 js and run webpack hot loader server
+ */
+gulp.task('compile-react-hot', function(done) {
+  webpackOptions.entry = [
+    'webpack-dev-server/client?' + WEBPACK_SERVER_HOST + ':' + WEBPACK_SERVER_PORT,
+    'webpack/hot/only-dev-server'
+  ].concat(webpackOptions.entry);
+  webpackOptions.plugins = [
+    new webpack.HotModuleReplacementPlugin({})
+  ];
+  webpackOptionsLoader.loaders.unshift('react-hot');
+  webpackOptions.output.publicPath = WEBPACK_SERVER_HOST + ':' + WEBPACK_SERVER_PORT + '/' + STATIC_PATH + '/';
+
+  new WebpackDevServer(webpack(webpackOptions), {
+    hot: true,
+    publicPath: '/' + STATIC_PATH + '/'
+  }).listen(WEBPACK_SERVER_PORT, WEBPACK_NETWORK_IP, function(err) {
+    if(err) console.log(err);
+    done();
+    console.log('webpack dev server listening at ' + WEBPACK_SERVER_HOST + ':' + WEBPACK_SERVER_PORT);
+  });
+});
+
+/**
+ * Clear previous html code from www
+ */
+gulp.task('clear-cordova-www', function () {
+  return gulp.src('www', {read: false})
+    .pipe(clean());
+});
+
+/**
+ * Fill cordova project with proper html, js, css
+ */
+gulp.task('prepare-build', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout', 'compile-react', done);
+});
+
+/**
+ * Emulate ios app with hot loader
+ */
+gulp.task('prebuild-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', done);
+});
+
+/**
+ * Emulate ios app with hot loader
+ */
+gulp.task('prebuild-ios-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', 'emulate-ios', done);
+});
+
+/**
+ * Emulate android app with hot loader
+ */
+gulp.task('prebuild-android-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', 'emulate-android', done);
+});
+
+/**
+ * Emulate browser app with hot loader
+ */
+gulp.task('prebuild-browser-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', 'run-browser', done);
+});
+
+/**
+ * Emulate app by ripple and hot loader
+ */
+gulp.task('prebuild-ripple-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', 'emulate-ripple', done);
+});
+
+/**
+ * Run test by 'The PhoneGap Developer App'
+ */
+gulp.task('prebuild-phonegap-hot', function(done) {
+  runSequence('clear-cordova-www', 'copy-layout-hot', 'compile-react-hot', 'phonegap-serve', done);
+});
